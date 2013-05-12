e2b8612757b654891f0495c03d1a271035924d69
samfisch <sf@notomorrow.de>
2013-05-12 16:31:52 +0200

    language

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,6 +1,12 @@
 <?php
+/*
+ * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)                                      
+ * @author     alice@notomorrow.de
+ * @author     zes@...
+ * @author    ...
+ */
 $lang['submit']      = 'SSL aktivieren';
-$lang['cancel']      = 'Aktion Abrechen';
+$lang['cancel']      = 'unverschlüsselte Übertragung';
 $lang['submittitle'] = 'Aktion fortsetzen';
 $lang['canceltitle'] = 'Zurück zur unverschlüsselten Version';
 $lang['certinfo']    = 'Die Verbindung wird mit dem Zertifikat für {{name}} signiert. Möglicherweise musst Du eine Ausnahme hinzufügen.';
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)                                      
+ * @author     alice@notomorrow.de
+ * @author     zes@...
+ * @author    ...
+ */
 $lang['submit']      = 'Change to SSL Encrytion';
 $lang['cancel']      = 'Return to plaintext transmission';
 $lang['submittitle'] = 'Continue Action';
