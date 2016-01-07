91ac87cbabde126cefbe0928172b26d730ffac39
Andras Tim <andras.tim@gmail.com>
2016-01-07 07:53:54 +0100

    lang: Added Hungary language

---
+++ lang/hu/lang.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)                                      
+ * @author     andras.tim@gmail.com
+ */
+$lang['submit']      = 'Váltás biztonságos kapcsolatra';
+$lang['cancel']      = 'Visszalépés';
+$lang['submittitle'] = 'Továbblépés biztonságos kapcsolat használatával';
+$lang['canceltitle'] = 'Művelet megszakítása és visszalépés';
+$lang['certinfo']    = 'A biztonságos kapcsolat tanúsítványát a <b>{{name}}</b> írta alá.';
+$lang['cainfo']      = 'Ha megbízik a tanúsítvány kiállítójában, töltse le a következő főtanúsítványt, és adja hozzá a böngészője <b>Legfelső szintű hitelesítő szolgáltatók</b> tárolójához!';
